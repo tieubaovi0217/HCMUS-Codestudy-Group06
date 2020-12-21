6722b4bda0803344867ad65979d1c3a86f184450
doanphuduc <doanphuducqh@gmail.com>
2020-12-21 17:09:13 +0700

    Add front end for view details problem

---
+++ src/components/viewproblem.editor.component.js
@@ -0,0 +1,33 @@
+import React, {useState, Component} from 'react';
+import { ControlledEditor } from "@monaco-editor/react";
+
+export default class Editor extends Component    {
+    constructor(props) {
+        super(props);
+        this.editorProps = {
+            mode: "cpp",
+            value: "",
+            autoheight: false
+        };
+    }
+    render() {
+        return (
+            <ControlledEditor
+                value = {this.editorProps.value}
+                language = {this.editorProps.mode}
+                height = {"70vh"}
+                options={{
+                    readOnly: false,
+                    scrollBeyondLastColumn: 0,
+                    scrollBeyondLastLine: false,
+                    minimap: {
+                      enabled: false
+                    },
+                    scrollbar: {
+                      alwaysConsumeMouseWheel: false
+                    }
+                  }}
+            />
+        );
+    }
+}
