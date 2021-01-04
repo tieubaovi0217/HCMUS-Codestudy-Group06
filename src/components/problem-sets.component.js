a80cff2ee694eb21d521e777aa13094f69d874c0
superman11399 <45527599+superman11399@users.noreply.github.com>
2021-01-04 10:44:17 +0700

    remove filter + unsolved

--- src/components/problem-sets.component.js
+++ src/components/problem-sets.component.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "../css/problem-sets.component.css";
 import { Link } from "react-router-dom";
-//import { MathComponent } from "mathjax-react";
 import axios from "axios";
 
 {
@@ -42,7 +41,7 @@ export default class ProblemSets extends Component {
 
   componentDidMount() {
     axios
-      .get("http://localhost:5000/problemset/")
+      .get("http://192.168.1.59:5000/problemset/")
       .then((response) => {
         this.setState({ problems: response.data });
         console.log(this.state.problems);
@@ -65,83 +64,6 @@ export default class ProblemSets extends Component {
       <div style={{ position: "relative" }}>
         <h3>View Problems</h3>
         {/*Filter*/}
-        <div id="sidebar">
-          <div className="roundbox sidebox _FilterByTagsFrame_main">
-            <div className="roundbox-lt">&nbsp;</div>
-            <div className="roundbox-rt">&nbsp;</div>
-            <div className="caption titled">
-              &rarr; Filter Problems
-              <div className="top-links"></div>
-            </div>
-            <div className="roundbox _FilterByTagsFrame_tagBoxPrototype">
-              <div className="roundbox-lt">&nbsp;</div>
-              <div className="roundbox-rt">&nbsp;</div>
-              <div className="roundbox-lb">&nbsp;</div>
-              <div className="roundbox-rb">&nbsp;</div>
-              <span
-                className="_FilterByTagsFrame_tagBoxItem"
-                data-name=""
-                title=""
-              >
-                <span className="_FilterByTagsFrame_tagBoxCaption">&nbsp;</span>{" "}
-                <img src="//sta.codeforces.com/s/58111/images/icons/close-10x10.png" />
-              </span>
-            </div>
-
-            <div style={{ padding: "0.5em" }}>
-              <form action="" method="get">
-                <div className="_FilterByTagsFrame_difficulty">
-                  <label>
-                    Difficulty:{" "}
-                    <span style={{ float: "right" }}>
-                      <input name="minDifficulty" value="" /> &mdash;{" "}
-                      <input name="maxDifficulty" value="" />
-                    </span>
-                  </label>
-                </div>
-
-                <div
-                  style={{ overflow: "auto" }}
-                  className="_FilterByTagsFrame_tagBoxes"
-                ></div>
-
-                <div className="_FilterByTagsFrame_addTag smaller">
-                  <label
-                    className="_FilterByTagsFrame_addTagLabel"
-                    style={{ display: "none" }}
-                  ></label>
-                  <a className="_FilterByTagsFrame_addTagLink">Add&nbsp;tag</a>
-                </div>
-
-                <div className="_FilterByTagsFrame_button">
-                  <input type="submit" value="Apply" />
-                </div>
-              </form>
-            </div>
-          </div>
-          {/*Last unsolved*/}
-          <div className="roundbox sidebox" style={{}}>
-            <div className="roundbox-lt">&nbsp;</div>
-            <div className="roundbox-rt">&nbsp;</div>
-            <div className="caption titled">
-              → Last unsolved
-              <div className="top-links"></div>
-            </div>
-            <table className="rtable smaller">
-              <tbody>
-                <tr>
-                  <th className="left " style={{ width: "2.25em" }}>
-                    #
-                  </th>
-                  <th className=" text-align-left">Name</th>
-                  <th className=" " style={{ width: "5em" }}>
-                    Last submission
-                  </th>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
 
         {/*Problems List*/}
         <div id="pageContent" class="content-with-sidebar">
