3a7f8ca2bf179a783c0339e2ca373cbffae6937f
Ubuntu <ubuntu@ip-172-31-14-118.us-east-2.compute.internal>
2021-03-22 06:19:33 +0000

    change BE

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -30,11 +38,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000", // <-- location of the react app were connecting to
-    credentials: true,
+    credentials: false,
   })
 );
-
 app.use(
   session({
     secret: "secretcode",
